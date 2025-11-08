eda59f1c0fd5eb963f5cf1dcdcf7be1ad5c1cfc4
naitokosuke <kosuke.naito.engineer@gmail.com>
2025-11-09 08:32:37 +0900

    feat: initialize renderer setup by importing createRenderer and nodeOps

--- packages/runtime-dom/index.ts
+++ packages/runtime-dom/index.ts
@@ -1,1 +1,6 @@
 // `renderer` のファクトリ関数の引数に `nodeOps`(Node 操作オブジェクト) を渡して、renderer オブジェクトを生成
+
+import { createRenderer } from "../runtime-core";
+import { nodeOps } from "./runtime-dom";
+
+const { render } = createRenderer(nodeOps);
