9db22dc573b7181eda528c7e8c0c19c6024410e8
naitokosuke <kosuke.naito.engineer@gmail.com>
2025-11-09 07:53:48 +0900

    feat: enhance renderer.ts with detailed comments and interface definitions

--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -1,2 +1,23 @@
-// render のロジックのみを持つオブジェクトを生成するためのファクトリ関数を定義
-// その際、Node(DOM に限らず)を扱うオブジェクトはファクトリの引数として受け取る
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+
+/*
+ * ============================================================================
+ * Renderer
+ * ============================================================================
+ *
+ * render のロジックのみを持つオブジェクトを生成するためのファクトリ関数を定義
+ * その際、Node(DOM に限らず)を扱うオブジェクトはファクトリの引数として受け取る
+ *
+ * ============================================================================
+ */
+
+export interface RendererOptions<HostNode = RendererNode> {
+  setElementText(node: HostNode, text: string): void;
+}
+
+export interface RendererNode {
+  [key: string]: any;
+}
+
+export interface RendererElement extends RendererNode { }
