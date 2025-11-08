33a81b01190728364c17f9ff169c4c37ce97a811
naitokosuke <kosuke.naito.engineer@gmail.com>
2025-11-09 00:22:39 +0900

    feat: implement createApp function and example usage

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,1 +1,8 @@
-import "chibivue";
+import { createApp } from "chibivue";
+
+// createApp は AppOptions 型の引数を受け取る必要がある
+const app = createApp({
+  render: () => "Hello World",
+});
+
+app.mount("#app");
--- packages/index.ts
+++ packages/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello, ChibiVue!");
+// const app = createApp({
+//   render() { return "Hello World"; },
+// });
+// app.mount();
+
+export type AppOptions = {
+  render: () => string;
+};
+export type App = {
+  mount: (selector: string) => void;
+};
+
+// App 型のオブジェクトを返す関数(ファクトリ関数)
+export const createApp = (options: AppOptions): App => {
+  return {
+    // mount とは 何かを DOM に描画すること
+    mount: selector => {
+      const root = document.querySelector(selector);
+      if (root) root.innerHTML = options.render();
+    },
+  };
+};
