22c0bd883e94cc40b27c1fb27dcd73b10baad182
naitokosuke <kosuke.naito.engineer@gmail.com>
2025-11-09 08:31:04 +0900

    feat: implement nodeOps with RendererOptions and enhance comments for clarity

--- packages/runtime-dom/nodeOps.ts
+++ packages/runtime-dom/nodeOps.ts
@@ -1,1 +1,9 @@
-// DOM 操作のためのオブジェクトの実装
+/* DOM 操作のためのオブジェクトの実装 */
+
+import { RendererOptions } from "../runtime-core";
+
+export const nodeOps: RendererOptions<Node> = {
+  setElementText(node, text) {
+    node.textContent = text;
+  },
+};
